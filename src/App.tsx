2c3052fd244b6896bee1b9a443fa0338101af0b8
JulieBenchuk <juliebenchuk@gmail.com>
2022-03-28 23:04:48 +0300

    added tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
 import './App.css';
+import ToDoList, {TaskType} from "./ToDoList";
 
 function App() {
+    const toDoList_1: string = "What to learn?";
+/*  const toDoList_2 = "Maybe that";
+    const toDoList_3 = "I don't know";*/
+
+    const tasks_1: TaskType [] = [
+        {id: 1, title:"HTML", isDone: true},
+        {id: 2, title:"CSS", isDone: true},
+        {id: 3, title:"JS/TS", isDone: true},
+    ];
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <ToDoList
+                title={toDoList_1}
+                tasks={tasks_1}
+            />
+            {/*            <ToDoList title={toDoList_2}/>
+            <ToDoList title={toDoList_3}/>*/}
         </div>
     );
 }
