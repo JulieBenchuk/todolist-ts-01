41d60e5cc98485c48365738d170ffa91e2247442
JulieBenchuk <juliebenchuk@gmail.com>
2022-03-29 11:00:49 +0300

    added second component (list What to buy)

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import './App.css';
 import ToDoList, {TaskType} from "./ToDoList";
 
 function App() {
-    const toDoList_1: string = "What to learn?";
-/*  const toDoList_2 = "Maybe that";
-    const toDoList_3 = "I don't know";*/
+    const toDoList_1: string = "What to learn";
+    const toDoList_2: string = "What to buy";
+  /*  const toDoList_3 = "I don't know";*/
 
     const tasks_1: TaskType [] = [
-        {id: 1, title:"HTML", isDone: true},
-        {id: 2, title:"CSS", isDone: true},
-        {id: 3, title:"JS/TS", isDone: true},
+        {id: 1, title: "HTML", isDone: true},
+        {id: 2, title: "CSS", isDone: true},
+        {id: 3, title: "JS/TS", isDone: false}
+    ];
+    const tasks_2: TaskType [] = [
+        {id: 1, title: "Buckwheat", isDone: false},
+        {id: 2, title: "Sugar", isDone: false},
+        {id: 3, title: "Office paper", isDone: true}
     ];
     return (
         <div className="App">
@@ -18,8 +23,10 @@ function App() {
                 title={toDoList_1}
                 tasks={tasks_1}
             />
-            {/*            <ToDoList title={toDoList_2}/>
-            <ToDoList title={toDoList_3}/>*/}
+            <ToDoList
+                title={toDoList_2}
+                tasks={tasks_2}
+            />
         </div>
     );
 }
