03ac3d69fecd0105cb6d4836042199aecb7dfc3e
JulieBenchuk <juliebenchuk@gmail.com>
2022-04-11 22:12:15 +0300

    add onClickButton & onChangeInput Handlers

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,62 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ToDoList, {TaskType} from "./ToDoList";
+import {v1} from "uuid";
+
+export type filterValuesType = "all" | "active" | "completed";
 
 function App() {
-    const toDoList_1: string = "What to learn";
-    const toDoList_2: string = "What to buy";
-  /*  const toDoList_3 = "I don't know";*/
+    console.log(v1());
 
-    const tasks_1: TaskType [] = [
-        {id: 1, title: "HTML", isDone: true},
-        {id: 2, title: "CSS", isDone: true},
-        {id: 3, title: "JS/TS", isDone: false}
-    ];
-    const tasks_2: TaskType [] = [
-        {id: 1, title: "Buckwheat", isDone: false},
-        {id: 2, title: "Sugar", isDone: false},
-        {id: 3, title: "Office paper", isDone: true}
-    ];
+    const toDoList: string = "What to learn";
+    let [tasks, setTasks] = useState<Array<TaskType>>([ //state, setState
+        {id: v1(), title: "HTML", isDone: true},
+        {id: v1(), title: "CSS", isDone: true},
+        {id: v1(), title: "JS/TS", isDone: false}
+    ]);
+
+    const onClickRemoveTask = (taskID: string) => {
+        const filteredTasksForRemove = tasks.filter(t => t.id !== taskID) //true>>new array
+        setTasks(filteredTasksForRemove) //set new condition of array TASKS
+    }
+    const AddTask = (title: string) => {
+        const newTask: TaskType = {
+            id: v1(),
+            title,    // аналогично title: title
+            isDone: false
+        }
+        const copyTasks = [...tasks]
+        copyTasks.push(newTask)
+        setTasks(copyTasks)
+
+    }
+
+    const [filter, setFilter] = useState<filterValuesType>("all")
+    const onClickChangeFilter = (filter: filterValuesType) => {
+        setFilter(filter)
+    }
+
+    let tasksForRender;
+    switch (filter) {
+        case "completed":
+            tasksForRender = tasks.filter(t => t.isDone === true);
+            break;
+        case "active":
+            tasksForRender = tasks.filter(t => t.isDone === false);
+            break;
+        default:
+            tasksForRender = tasks;
+    }
+
     return (
         <div className="App">
             <ToDoList
-                title={toDoList_1}
-                tasks={tasks_1}
-            />
-            <ToDoList
-                title={toDoList_2}
-                tasks={tasks_2}
+                title={toDoList}
+                tasks={tasksForRender}
+                addTask={AddTask}
+                removeTask={onClickRemoveTask}
+                changeFilter={onClickChangeFilter}
+
             />
         </div>
     );
--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,36 +1,72 @@
-import React from "react";
+import React, {ChangeEvent, DetailedHTMLProps, InputHTMLAttributes, useState} from "react";
+import {filterValuesType} from "./App";
 
 type ToDoListPropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (taskID: string) => void
+    changeFilter: (filter: filterValuesType) => void
+    addTask: (title: string) => void
 }
 export type TaskType = {
-    id: number
+    id: string
     title: string
     isDone: boolean
 }
 export const ToDoList = (props: ToDoListPropsType) => {
-    return (
-        <>
-            <div>
-                <h3>{props.title}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-                    <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-                    <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+    const [input, setInput] = useState<string>(" ")
+    const onClickAddTaskHandler = () => {
+        const trimmedInput = input.trim()  //удаляем пробелы
+        if (trimmedInput) {     //когда не пустая строка-true
+            props.addTask(input)
+        }
+        setInput("")
+    }
+    const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setInput(e.currentTarget.value)
+    }
+    const onKeyPressHandler = (e: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>) => {
+        if (e.key === "Enter") onClickAddTaskHandler()
+    }
+    const onAllClickHandler = () => {
+        props.changeFilter("all")
+    }
+    const onActiveClickHandler = () => {
+        props.changeFilter("active")
+    }
+    const onCompletedClickHandler = () => {
+        props.changeFilter("completed")
+    }
 
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
+    const tasksListItems = props.tasks.map(t => {
+        const onClickHandler = () => props.removeTask(t.id)
+        return <li key={t.id}>
+                <input type="checkbox" checked={t.isDone}/>
+                <span>{t.title}</span>
+                <button onClick={onClickHandler}>delete
+                </button>
+            </li>
+    })
+
+    return (
+        <div>
+            <h3>{props.title}</h3>
+            <div>
+                <input value={input}
+                       onChange={onChangeInputHandler}
+                       onKeyPress={onKeyPressHandler}
+                />
+                <button onClick={onClickAddTaskHandler}
+                >+
+                </button>
             </div>
-        </>
+            <ul>{tasksListItems}</ul>
+            <div>
+                <button onClick={onAllClickHandler}>All</button>
+                <button onClick={onActiveClickHandler}>Active</button>
+                <button onClick={onCompletedClickHandler}>Completed</button>
+            </div>
+        </div>
     );
 }
 export default ToDoList;
